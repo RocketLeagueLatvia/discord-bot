bf098be3166e81e3fc714f9a1bcaeec1b5de5139
pundurs <pundurs@glhf.lv>
2017-11-19 23:45:26 +0200

    Finished tests for lib/event.
    Added remaining tests for lib/event.
    Changed all equality assertions to strict instead of absctract.
    Fixed where test data wasn't deleted if any of the assertions fail.
    Fixed where hasRegistered() didn't work at all as intended.
    Ensured getRegisteredPlayers() and getCheckedInPlayers() returns an
    array of Player instances instead of plain objects.

--- test/event.js
+++ test/event.js
@@ -1,6 +1,7 @@
 const assert = require('assert');
 const rewire = require('rewire');
 const event = rewire('../lib/event');
+const Player = require('../lib/player');
 const { getClient } = require('./lib/mongo');
 
 const getEventClient = function () {
@@ -17,50 +18,69 @@ const deleteEvents = function (...ids) {
     }});
 };
 
-describe('Event', function() {
+const createPlayer = function () {
+    return new Player({
+        discordid: '1234567890',
+        discordnick: 'testplayer',
+        steamid64: '1234567890',
+        maxmmr: 1000
+    });
+};
 
+describe('Event', function() {
     describe('#create()', function() {
+        let e;
         it('should create a new Event', async function () {
             let eventName = 'Test event',
                 collection = getCollection(),
-                eventCount = await collection.count(),
-                e;
+                eventCount = await collection.count();
 
             e = await event.create(eventName);
 
-            assert.equal(await collection.count(), eventCount + 1, 'Event count doesn\'t match');
-            assert.equal(eventName, e.name, 'Event was created with incorrect name');
+            assert.strictEqual(await collection.count(), eventCount + 1, 'Event count doesn\'t match');
+            assert.strictEqual(eventName, e.name, 'Event was created with incorrect name');
             assert.ok(e instanceof event, 'create() didn\'t return an Event instance');
+        });
 
+        after(function () {
             deleteEvents(e._id);
         });
     });
 
     describe('#findById()', function() {
+        let e, e2;
         it('should be able to find an event by an ID', async function () {
-            let e = await event.create('Test event');
-            let e2 = await event.findById(e._id);
-            deleteEvents(e._id, e2._id);
+            e = await event.create('Test event');
+            e2 = await event.findById(e._id);
 
-            assert.equal(e._id.toString(), e2._id.toString(), 'Event names don\'t match');
+            assert.strictEqual(e._id.toString(), e2._id.toString(), 'Event names don\'t match');
             assert.ok(e2 instanceof event, 'findById() didn\'t return an Event instance');
 
+        });
+
+        after(function () {
+            deleteEvents(e._id, e2._id);
         });
     });
 
     describe('#findByName()', function() {
+        let e;
         it('should be able to find an event by an name', async function () {
             let eventName = 'Test event';
-            let e = await event.create(eventName);
+            e = await event.create(eventName);
             let e2 = await event.findByName(eventName);
-            deleteEvents(e._id);
 
-            assert.equal(e.name, e2.name, 'Names do not match.');
+            assert.strictEqual(e.name, e2.name, 'Names do not match.');
             assert.ok(e2 instanceof event, 'findByName doesn\'t return an event');
+        });
+
+        after(function () {
+            deleteEvents(e._id);
         });
     });
 
     describe('#findCurrentEvent()', function() {
+        let eCurrent;
         it('should be able to find the latest open and visible event', async function () {
             // TODO: Refactor this when event creation timestamps are added.
             let e = await event.create('Test event');
@@ -68,21 +88,25 @@ describe('Event', function() {
             await e.show();
             await e.openRegistration();
 
-            let eCurrent = await event.findCurrentEvent();
+            eCurrent = await event.findCurrentEvent();
 
-            assert.equal(eCurrent._id.toString(), e._id.toString(), 'Returned event isn\'t the current one');
+            assert.strictEqual(eCurrent._id.toString(), e._id.toString(), 'Returned event isn\'t the current one');
             assert.ok(eCurrent instanceof event, 'findCurrentEvent() didn\'t return an Event instance');
+        });
 
+        after(function () {
             deleteEvents(eCurrent._id);
         });
     });
 
     describe('#findVisible()', function() {
+        let e;
         it('should be able to find only visible events', async function () {
             let collection = getCollection();
             let visibleEventCount = await collection.count({ visible: true });
-            let e = await event.create('Test event');
             let e2;
+
+            e = await event.create('Test event');
 
             await collection.updateOne({ _id: e._id }, {
                 $set: {visible: false}
@@ -90,7 +114,7 @@ describe('Event', function() {
 
             let visibleEvents = await event.findVisible();
 
-            assert.equal(visibleEventCount, visibleEvents.length, 'Visible event count is incorrect');
+            assert.strictEqual(visibleEventCount, visibleEvents.length, 'Visible event count is incorrect');
 
             // Update created event to visible and check if "findVisible()" contains Event instances
             // That way we also check if newest event is first
@@ -101,77 +125,92 @@ describe('Event', function() {
             visibleEvents = await event.findVisible();
             e2 = visibleEvents[0];
 
-            assert.equal(e._id.toString(), e2._id.toString(), 'Incorrect ordering of events returned');
+            assert.strictEqual(e._id.toString(), e2._id.toString(), 'Incorrect ordering of events returned');
             assert.ok(e2 instanceof event, 'findVisible() doesn\'t return Event instances');
+        });
 
+        after(function () {
             deleteEvents(e._id);
-
         });
     });
 
     describe('#openRegistration()', function() {
+        let e;
         it('should open registrations for the current event', async function () {
-            let e = await event.create('Test event');
             let collection = getCollection();
+
+            e = await event.create('Test event');
 
             await collection.updateOne({ _id: e._id }, {
                 $set: {'status.registration': event.STATUS.CLOSED}
             });
             await e.openRegistration();
 
-            assert.equal(e.status.registration, event.STATUS.OPEN);
+            assert.strictEqual(e.status.registration, event.STATUS.OPEN);
 
             // Check if database entry corresponds to the Event object
             let e2 = await collection.findOne({ _id: e._id });
-            assert.equal(e.status.registration, e2.status.registration);
+            assert.strictEqual(e.status.registration, e2.status.registration);
 
+        });
+
+        after(function () {
             deleteEvents(e._id);
         });
     });
 
     describe('#closeRegistration()', function() {
+        let e;
         it('should close registrations for the current event', async function () {
-            let e = await event.create('Test event');
             let collection = getCollection();
+
+            e = await event.create('Test event');
 
             await collection.updateOne({ _id: e._id }, {
                 $set: {'status.registration': event.STATUS.OPEN}
             });
             await e.closeRegistration();
 
-            assert.equal(e.status.registration, event.STATUS.CLOSED);
+            assert.strictEqual(e.status.registration, event.STATUS.CLOSED);
 
             // Check if database entry corresponds to the Event object
             let e2 = await collection.findOne({ _id: e._id });
-            assert.equal(e.status.registration, e2.status.registration);
+            assert.strictEqual(e.status.registration, e2.status.registration);
+        });
 
+        after(function () {
             deleteEvents(e._id);
         });
     });
 
     describe('#openCheckIn()', function() {
+        let e;
         it('should open checkIn for the current event', async function () {
-            let e = await event.create('Test event');
             let collection = getCollection();
+
+            e = await event.create('Test event');
 
             await collection.updateOne({ _id: e._id }, {
                 $set: {'status.checkIn': event.STATUS.CLOSED}
             });
             await e.openCheckIn();
 
-            assert.equal(e.status.checkIn, event.STATUS.OPEN);
+            assert.strictEqual(e.status.checkIn, event.STATUS.OPEN);
 
             // Check if database entry corresponds to the Event object
             let e2 = await collection.findOne({ _id: e._id });
-            assert.equal(e.status.checkIn, e2.status.checkIn);
+            assert.strictEqual(e.status.checkIn, e2.status.checkIn);
+        });
 
+        after(function () {
             deleteEvents(e._id);
         });
     });
 
     describe('#closeCheckIn()', function() {
+        let e;
         it('should close checkIn for the current event', async function () {
-            let e = await event.create('Test event');
+            e = await event.create('Test event');
             let collection = getCollection();
 
             await collection.updateOne({ _id: e._id }, {
@@ -179,19 +218,24 @@ describe('Event', function() {
             });
             await e.closeCheckIn();
 
-            assert.equal(e.status.checkIn, event.STATUS.CLOSED);
+            assert.strictEqual(e.status.checkIn, event.STATUS.CLOSED);
 
             // Check if database entry corresponds to the Event object
             let e2 = await collection.findOne({ _id: e._id });
-            assert.equal(e.status.checkIn, e2.status.checkIn);
+            assert.strictEqual(e.status.checkIn, e2.status.checkIn);
 
+            deleteEvents(e._id);
+        });
+
+        after(function () {
             deleteEvents(e._id);
         });
     });
 
     describe('#show()', function() {
+        let e;
         it('should set the current event to visible', async function () {
-            let e = await event.create('Test event');
+            e = await event.create('Test event');
             let collection = getCollection();
 
             await collection.updateOne({ _id: e._id }, {
@@ -199,19 +243,24 @@ describe('Event', function() {
             });
             await e.show();
 
-            assert.equal(e.visible, true);
+            assert.strictEqual(e.visible, true);
 
             // Check if database entry corresponds to the Event object
             let e2 = await collection.findOne({ _id: e._id });
-            assert.equal(e.visible, e2.visible);
+            assert.strictEqual(e.visible, e2.visible);
 
+            deleteEvents(e._id);
+        });
+
+        after(function () {
             deleteEvents(e._id);
         });
     });
 
     describe('#hide()', function() {
+        let e;
         it('should set the current event to hidden', async function () {
-            let e = await event.create('Test event');
+            e = await event.create('Test event');
             let collection = getCollection();
 
             await collection.updateOne({ _id: e._id }, {
@@ -219,38 +268,134 @@ describe('Event', function() {
             });
             await e.hide();
 
-            assert.equal(e.visible, false);
+            assert.strictEqual(e.visible, false);
 
             // Check if database entry corresponds to the Event object
             let e2 = await collection.findOne({ _id: e._id });
-            assert.equal(e.visible, e2.visible);
+            assert.strictEqual(e.visible, e2.visible);
+        });
 
+        after(function () {
             deleteEvents(e._id);
         });
     });
 
-    describe('#hasRegistered()', function() {
-        it('should check if the player supplied is registered');
+    describe('Player registration', function () {
+        let e;
+        let player = createPlayer();
+        let player2;
+        describe('#register()', function() {
+            it('should register the supplied player', async function () {
+                let eventPlayer;
+
+                e = await event.create('Test event');
+
+                await e.register(player);
+
+                eventPlayer = e.players[0];
+                assert.strictEqual(eventPlayer.discordid, player.discordid);
+
+                // Check if database entry corresponds to the Event object
+                let e2 = await getCollection().findOne({ _id: e._id });
+                assert.strictEqual(e2.players.length, 1);
+                assert.strictEqual(e2.players[0].discordid, player.discordid);
+            });
+        });
+
+        describe('Registration checking', function () {
+            // Check a non-registered player
+            player2 = createPlayer();
+            player2.discordid = 'xxx';
+            describe('#hasRegistered()', function() {
+                it('should check if the player supplied is registered', async function () {
+                    // Check an actually registered player
+                    assert.ok(await e.hasRegistered(player));
+
+                    // Check an unregistered one
+                    assert.ok(!await e.hasRegistered(player2));
+                });
+            });
+
+            describe('#playerIsRegistered()', function() {
+                it('should return if the supplied player is registered', async function () {
+                    // Check an actually registered player
+                    assert.ok(await e.playerIsRegistered(player.discordid));
+
+                    // Check an unregistered one
+                    assert.ok(!await e.playerIsRegistered(player2.discordid));
+                });
+            });
+        });
+
+        describe('#getRegisteredPlayers()', function() {
+            it('should return the players registered for this event sorted by descending MMR', async function () {
+                let registeredPlayers;
+                let first;
+
+                player2.maxmmr = 1;
+                await e.register(player2);
+                registeredPlayers = await e.getRegisteredPlayers();
+
+                first = registeredPlayers[0];
+                assert.ok(first instanceof Player);
+                assert.strictEqual(first.discordid, player.discordid);
+                assert.strictEqual(registeredPlayers[1].discordid, player2.discordid);
+            });
+        });
+
+        after(function () {
+            deleteEvents(e._id);
+        });
     });
 
-    describe('#register()', function() {
-        it('should register the supplied player');
-    });
+    describe('Player checkin', function () {
+        let e;
+        let player = createPlayer();
+        let player2 = createPlayer();
+        let player3 = createPlayer();
 
-    describe('#checkIn()', function() {
-        it('should checkin the supplied player');
-    });
+        player2.maxmmr = 1;
+        player2.discordid = 'XXX';
+        player3.discordid = 'YYY';
 
-    describe('#getRegisteredPlayers()', function() {
-        it('should return the players registered for this event');
-    });
+        before(async function () {
+            e = await event.create('Test event');
+            await e.register(player);
+            await e.register(player2);
+            await e.register(player3);
+        });
 
-    describe('#getCheckedInPlayers()', function() {
-        it('should return the players registered for this event');
-    });
+        describe('#checkIn()', function() {
+            it('should checkin the supplied player', async function () {
+                await e.checkIn(player);
+                let eventWithCheckedInPlayer = await getCollection().findOne({
+                    '_id': e._id,
+                    'players.discordid': player.discordid,
+                    'players.checkedIn': true
+                });
 
-    describe('#playerIsRegistered()', function() {
-        it('should return if the supplied player is registered');
+                assert.ok(eventWithCheckedInPlayer);
+
+            });
+        });
+
+        describe('#getCheckedInPlayers()', function() {
+            it('should return the players checked in for this event ordered by descending MMR', async function () {
+                let checkedInPlayers;
+                await e.checkIn(player2);
+                checkedInPlayers = await e.getCheckedInPlayers();
+
+                let first = checkedInPlayers[0];
+                assert.ok(first instanceof Player);
+                assert.strictEqual(first.discordid, player.discordid);
+                assert.strictEqual(checkedInPlayers[1].discordid, player2.discordid);
+                assert.strictEqual(checkedInPlayers.length, 2);
+            });
+        });
+
+        after(function () {
+            deleteEvents(e._id);
+        });
     });
 
     after(function () {
