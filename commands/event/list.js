80cbc64745d3390c0e636647067e0dfaf79bf3f6
pundurs <pundurs@glhf.lv>
2017-11-22 00:10:02 +0200

    Added upcoming event list command code.

--- commands/event/list.js
+++ commands/event/list.js
@@ -1,4 +1,6 @@
+const Discord = require('discord.js');
 const { Command } = require('discord.js-commando');
+const Event = require('../../lib/event');
 
 module.exports = class ListCommand extends Command {
     constructor(client) {
@@ -17,7 +19,28 @@ module.exports = class ListCommand extends Command {
     }
 
     async run(msg) {
-        // todo: replace with formatted event list
-        return msg.say(`Sorry, I can't remember any events.`);
+        const events = await Event.findVisible();
+
+        if (!events.length) return msg.say('Sorry, there are no upcoming events.'); 
+
+        const embed = new Discord.RichEmbed();
+        embed.setTitle('Upcoming events');
+        embed.setColor(0x4549cc);
+
+        let eventNames = [];
+        let eventStatusesRegistration = [];
+        let eventStatusesCheckIn = [];
+
+        events.forEach(ev => {
+            eventNames.push(ev.name);
+            eventStatusesRegistration.push(ev.status.registration);
+            eventStatusesCheckIn.push(ev.status.checkIn);
+        });
+
+        embed.addField('Name', eventNames.join('\n'), true);
+        embed.addField('Registration', eventStatusesRegistration.join('\n'), true);
+        embed.addField('Check in', eventStatusesCheckIn.join('\n'), true);
+
+        return msg.channel.send({embed: embed});
     }
 };
